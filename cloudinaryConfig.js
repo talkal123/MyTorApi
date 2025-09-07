2297996163cc8f67185146570aa35a046f280ba9
talkal123 <talkal153@gmail.com>
2025-09-07 15:01:01 +0300

    new commit

--- cloudinaryConfig.js
+++ cloudinaryConfig.js
@@ -1,11 +1,12 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
+const env = require('dotenv').config();
 
 cloudinary.config({
-  cloud_name: "dwzs8l6ta",
-  api_key: "584758399419148",
-  api_secret: "ZwtTWwm3SHH5bY3o6Y12NVWbGXs",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY_CLOUDINARY,
+  api_secret: process.env.API_KEY_CLOUDINARY_SECRET,
 });
 
 const storage = new CloudinaryStorage({

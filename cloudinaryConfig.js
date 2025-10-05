f6cc03ad78ce8fa148c6fe8b09834a6c3a13753b
talkal123 <talkal153@gmail.com>
2025-10-05 22:18:12 +0300

    a

--- cloudinaryConfig.js
+++ cloudinaryConfig.js
@@ -1,21 +1,21 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
-require("dotenv").config(); // רק טוען את הקובץ, לא צריך לשמור במשתנה
 
+// קונפיג של Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY_CLOUDINARY,
   api_secret: process.env.API_KEY_CLOUDINARY_SECRET,
 });
 
+// MulterStorage מקבל instance מקומי בלבד
 const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "userPhotos",
-  },
+  cloudinary: cloudinary,
+  params: { folder: "userPhotos" }
 });
 
+// multer עם storage
 const upload = multer({ storage });
 
-module.exports = { upload };
+module.exports = { upload }; // מייצא רק upload
